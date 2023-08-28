4da845b4fd0727a795a626d17b0bd8a728c67113
hanseulhee <3021062@gmail.com>
2023-08-29 04:21:12 +0900

    react-email settings

---
+++ emails/index.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@react-email/button";
+import { Html } from "@react-email/html";
+import * as React from "react";
+
+export default function Email() {
+  return (
+    <Html>
+      <Button></Button>
+    </Html>
+  );
+}
